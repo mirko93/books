ff087283799d8a2a5d1c936673dcae319a825634
mirko93 <45762923+mirko93@users.noreply.github.com>
2020-05-20 11:56:28 +0200

    adds author resource

---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Author;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthorManagementTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function an_author_can_be_created()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/author', [
+           'name' => 'Author Name',
+           'dob' => '11/13/1993',
+        ]);
+
+        $author = Author::all();
+
+        $this->assertCount(1, $author);
+        $this->assertInstanceOf(Carbon::class, $author->first()->dob);
+        $this->assertEquals('1993/13/11', $author->first()->dob->format('Y/d/m'));
+    }
+}
