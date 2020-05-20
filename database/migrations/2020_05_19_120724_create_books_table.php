6c823dd5bbe4e37f0aa2fcc652b6b6115d3e1755
mirko93 <45762923+mirko93@users.noreply.github.com>
2020-05-20 12:52:08 +0200

    adds new columns

--- database/migrations/2020_05_19_120724_create_books_table.php
+++ database/migrations/2020_05_19_120724_create_books_table.php
@@ -15,8 +15,8 @@ class CreateBooksTable extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('author_id');
             $table->string('title');
-            $table->string('author');
             $table->timestamps();
         });
     }
