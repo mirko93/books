b175273c08a99652ed9521016e86dee0018e0811
mirko93 <45762923+mirko93@users.noreply.github.com>
2020-05-21 15:49:10 +0200

    wip for authors and change route from author to authors

--- app/Http/Controllers/AuthorsController.php
+++ app/Http/Controllers/AuthorsController.php
@@ -9,8 +9,17 @@ class AuthorsController extends Controller
 {
     public function store()
     {
-        Author::create(\request()->only([
-            'name', 'dob',
-        ]));
+        Author::create($this->validateRequest());
+    }
+
+    /**
+     * @return array
+     */
+    private function validateRequest(): array
+    {
+        return \request()->validate([
+            'name' => 'required',
+            'dob' => 'required',
+        ]);
     }
 }
